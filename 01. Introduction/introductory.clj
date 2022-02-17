41ce9a15d5e39e891c27f18e3875cbb6e5a8e72b
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-02-17 13:11:58 -0600

    Crea archivo con solución a problemas 1, 2 y 4 del problemario 1

---
+++ 01. Introduction/introductory.clj
@@ -0,0 +1,48 @@
+; Un comentario
+
+(ns introductory
+  (:require [clojure.test :refer [deftest is run-tests]])
+  (:require [clojure.math.numeric-tower :refer [sqrt]]))
+
+;----------------------------------------------------------
+; Problem 1
+(defn gibibytes->bytes
+  [gibibytes]
+  (* gibibytes 1024 1024 1024))
+
+(deftest test-gibibytes->bytes
+  (is (= 0 (gibibytes->bytes 0)))
+  (is (= 1073741824 (gibibytes->bytes 1)))
+  (is (= 5368709120 (gibibytes->bytes 5)))
+  (is (= 26415122612224 (gibibytes->bytes 24601))))
+
+;----------------------------------------------------------
+; Problem 2
+(defn f2c
+  [F]
+  (/ (* 5
+        (- F 32))
+     9))
+
+(deftest test-f2c
+  (is (= 100.0 (f2c 212.0)))
+  (is (= 0.0 (f2c 32.0)))
+  (is (= -40.0 (f2c -40.0))))
+
+;----------------------------------------------------------
+; Problem 4
+(defn roots
+  [a b c]
+  (let [d (- b)
+        e (sqrt (- (* b b) (* 4 a c)))
+        f (* 2 a)]
+    [(/ (+ d e) f)
+     (/ (- d e) f)]))
+
+(deftest test-roots
+  (is (= [-1 -1] (roots 2 4 2)))
+  (is (= [0 0] (roots 1 0 0)))
+  (is (= [-1/4 -1] (roots 4 5 1))))
+
+;----------------------------------------------------------
+(run-tests)
