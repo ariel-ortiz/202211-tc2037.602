b366d3efa51bf430010a4d3e3e3f46a86ccef1fc
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-03-10 14:17:58 -0600

    Implementa solución a problemas 8 y 10 del problemario 3

---
+++ 02. Repeticiones/more_repetitions.clj
@@ -0,0 +1,66 @@
+; Problem Set #3
+
+(ns more-repetitions
+  (:require [clojure.test :refer [deftest is run-tests]]))
+
+
+;----------------------------------------------------------
+; Problem 8
+
+(defn insert-everywhere-aux
+  [x s index]
+  (let [[antes despues] (split-at index s)]
+    (concat antes [x] despues)))
+
+(insert-everywhere-aux 1 '(a b c d e) 10)
+
+(defn insert-everywhere
+  [x s]
+  (->> (range (inc (count s)))
+       (map #(insert-everywhere-aux x s %))))
+
+(insert-everywhere 1 '(a b c d e))
+
+(deftest test-insert-everywhere
+  (is (= '((1)) (insert-everywhere 1 ())))
+  (is (= '((1 a) (a 1)) (insert-everywhere 1 '(a))))
+  (is (= '((1 a b c) (a 1 b c) (a b 1 c) (a b c 1))
+         (insert-everywhere 1 '(a b c))))
+  (is (= '((1 a b c d e)
+           (a 1 b c d e)
+           (a b 1 c d e)
+           (a b c 1 d e)
+           (a b c d 1 e)
+           (a b c d e 1))
+         (insert-everywhere 1 '(a b c d e))))
+  (is (= '((x 1 2 3 4 5 6 7 8 9 10)
+           (1 x 2 3 4 5 6 7 8 9 10)
+           (1 2 x 3 4 5 6 7 8 9 10)
+           (1 2 3 x 4 5 6 7 8 9 10)
+           (1 2 3 4 x 5 6 7 8 9 10)
+           (1 2 3 4 5 x 6 7 8 9 10)
+           (1 2 3 4 5 6 x 7 8 9 10)
+           (1 2 3 4 5 6 7 x 8 9 10)
+           (1 2 3 4 5 6 7 8 x 9 10)
+           (1 2 3 4 5 6 7 8 9 x 10)
+           (1 2 3 4 5 6 7 8 9 10 x))
+         (insert-everywhere 'x '(1 2 3 4 5 6 7 8 9 10)))))
+
+;----------------------------------------------------------
+; Problem 10
+
+(defn pack
+  [s]
+  (partition-by identity s))
+
+(pack [1 1 1 1 5 5 5 2 7 7 3 3 3 3])
+
+(deftest test-pack
+  (is (= () (pack ())))
+  (is (= '((a a a a) (b) (c c) (a a) (d) (e e e e))
+         (pack '(a a a a b c c a a d e e e e))))
+  (is (= '((1) (2) (3) (4) (5)) (pack '(1 2 3 4 5))))
+  (is (= '((9 9 9 9 9 9 9 9 9)) (pack '(9 9 9 9 9 9 9 9 9)))))
+
+
+(run-tests)
