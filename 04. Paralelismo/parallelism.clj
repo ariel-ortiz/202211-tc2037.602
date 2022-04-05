166cda8ae2ee01f507d2f88561d3336c95558b36
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-04-04 22:50:39 -0500

    Implementa completamente problema 1 del problemario 5

--- 04. Paralelismo/parallelism.clj
+++ 04. Paralelismo/parallelism.clj
@@ -19,4 +19,36 @@
         (recur (inc i)
                (*' i r))))))
 
-(fact-seq 1000)
+(defn fact-range
+  [[start end]]
+  (loop [i start
+         r 1]
+    (if (= i end)
+      r
+      (recur (inc i)
+             (*' i r)))))
+
+(defn compute-ranges
+  [n p]
+  (partition 2
+             1
+             (concat (range 1 n (quot n p))
+                     [(inc n)])))
+
+(defn fact-par
+  [n]
+  (->>
+    (.availableProcessors (Runtime/getRuntime))
+    (compute-ranges n)
+    (pmap fact-range)
+    (reduce *')
+    (bits)))
+
+(def n 200000)
+(time (fact-seq n))
+(time (fact-par n))
+
+;;; T1 = 13534.6525
+;;; Tp = 632.058
+;;; p = 8
+;;; Sp = 13534.6525 / 632.058 = 21.413624224359157
