0ad6c4a283f2be2cb0c86108c373640a0f98b6c4
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-06-09 13:08:11 -0500

    Implementa solución al problema 1 del problemario 9

--- 09. Máquinas de Turing/turing_machines.clj
+++ 09. Máquinas de Turing/turing_machines.clj
@@ -1,3 +1,5 @@
+; Problem Set #9
+
 (ns turing-machines
   (:require [clojure.test :refer [deftest is run-tests]])
   (:import (java.io Writer)))
@@ -12,8 +14,67 @@
   [self ^Writer writer]
   (.write writer (str self)))
 
+(defn make-tape
+  ([s]
+   (make-tape "" (if (= s "") \_ (first s)) (rest s)))
+  ([left head right]
+   (let [left (drop-while #(= % \_) left)
+         right (reverse (drop-while #(= % \_) (reverse right)))]
+     (->Tape (apply str left)
+             head
+             (apply str right)))))
+
+(defn write-tape
+  [{:keys [left right]} value]
+  (make-tape left value right))
+
+(write-tape (make-tape "aaa" \a "aaaa" ) \x)
+
+(defn shift-head
+  [{:keys [left head right]} direction]
+  (case direction
+    :right (make-tape (str left head)
+                      (or (first right) \_)
+                      (rest right))
+    :left (make-tape (butlast left)
+                     (or (last left) \_)
+                     (str head right))
+    (throw (ex-info (str "Bad direction: " direction) {}))))
+
+(defn accepts
+  [{:keys [initial-state accept-states transitions]} input]
+  (loop [{:keys [head] :as tape} (make-tape input)
+         current-state initial-state]
+    ;(println current-state tape)
+    (if (contains? accept-states current-state)
+      (str tape)
+      (if-let [[symbol direction next-state]
+               ((transitions current-state) head)]
+        (recur (shift-head (write-tape tape symbol) direction)
+               next-state)
+        nil))))
+
+;----------------------------------------------------------
+; Problem 1
+
 (def tm-1 (->TM :q0
                 #{:q2}
                 {:q0 {\a [\a :right :q1]
                       \_ [\_ :left :q2]}
                  :q1 {\a [\a :right :q0]}}))
+
+(deftest test-problem1
+  (is (= "[_]"
+         (accepts tm-1 "")))
+  (is (= "a[a]"
+         (accepts tm-1 "aa")))
+  (is (= "aaaaaaa[a]"
+         (accepts tm-1 "aaaaaaaa")))
+  (is (= "aaaaaaaaaaaaaaaaaaaaaaaaa[a]"
+         (accepts tm-1 "aaaaaaaaaaaaaaaaaaaaaaaaaa")))
+  (is (nil? (accepts tm-1 "a")))
+  (is (nil? (accepts tm-1 "aaa")))
+  (is (nil? (accepts tm-1 "aaaaaaa")))
+  (is (nil? (accepts tm-1 "aaaaaaaaaaaaaaaaaaaaaaaaa"))))
+
+(run-tests)
